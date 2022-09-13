668c4c648ce6e0c585dc25ef847b1fbec3c20b10
jericho <jericho_ph@qq.com>
2022-09-13 18:32:25 +0800

    来源类型&来源名称

--- app/Models/SourceName.php
+++ app/Models/SourceName.php
@@ -3,8 +3,36 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Carbon;
 
+/**
+ * Class SourceName
+ * @package App\Models
+ * @property int $id
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property Carbon|null $deleted_at
+ * @property string $uuid
+ * @property int $sort
+ * @property string $unique_code
+ * @property string $name
+ * @property string $source_type_uuid
+ * @property-read SourceType $source_type
+ */
 class SourceName extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $guarded = [];
+
+    /**
+     * 所属来源类型
+     * @return HasOne
+     */
+    public function SourceType():HasOne
+    {
+        return $this->hasOne(SourceType::class,"uuid","source_type_uuid");
+    }
 }
--- app/Models/SourceType.php
+++ app/Models/SourceType.php
@@ -3,8 +3,35 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Carbon;
 
+/**
+ * Class SourceType
+ * @package App\Models
+ * @property int $id
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property Carbon|null $deleted_at
+ * @property string $uuid
+ * @property int $sort
+ * @property string $unique_code
+ * @property string $name
+ * @property Sourcename[] $source_names
+ */
 class SourceType extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $guarded = [];
+
+    /**
+     * 相关来源名称
+     * @return BelongsTo
+     */
+    public function SourceNames(): BelongsTo
+    {
+        return $this->belongsTo(SourceName::class, "source_type_uuid", "uuid");
+    }
 }
