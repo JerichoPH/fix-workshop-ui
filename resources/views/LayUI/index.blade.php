e9ca2f175ce8063ac3108700e10ffa197fc14085
jericho <jericho_ph@qq.com>
2022-09-20 16:14:12 +0800

    m

---
+++ resources/views/LayUI/index.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="/layui/css/layui.css" rel="stylesheet">
+    <script src="/layui/layui.jss"></script>
+    <title>{{ env("APP_NAME") }}</title>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
